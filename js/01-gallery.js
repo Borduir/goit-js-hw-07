22f02b61189014c7ec6b1645dd38b55634ead776
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-21 18:38:26 +0300

    fixed 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,22 @@
+"use strict";
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const gallery = document.querySelector(".gallery");
+const imageList = [];
+for (const galleryItem of galleryItems) {
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", `${galleryItem.original}`);
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.setAttribute("src", `${galleryItem.preview}`);
+  img.setAttribute("data-source", `${galleryItem.original}`);
+  img.setAttribute("alt", `${galleryItem.description}`);
+  imageList.push(link);
+  link.appendChild(img);
+}
+gallery.append(...imageList);
+
 gallery.addEventListener("click", selectGalleryItem);
 
 function selectGalleryItem(e) {
