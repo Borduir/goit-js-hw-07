2d31c69a728cd05da8cfc478d02acff7117f1a5e
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-09-21 17:44:06 +0300

    task 1 completed(without optional)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", selectGalleryItem);
+
+function selectGalleryItem(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  basicLightbox
+    .create(
+      `<img width="1400" height="900" src=${e.target.getAttribute(
+        "data-source"
+      )}>`
+    )
+    .show();
+}
+
+// gallery.addEventListener("keydown", closeModal);
+// function closeModal(e) {
+//   if (e.key === "Escape") {
+//     basicLightbox.;
+//   }
+// }
